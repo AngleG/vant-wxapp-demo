55fbf7ebc7831fa9482e79eb19d21ab6068048c9
wxx <1464661791@qq.com>
2020-06-11 10:47:18 +0800

    编辑收款信息组件，支持获取数据后回显功能，支持新增和编辑

--- src/components/edit-payment/edit-payment.ts
+++ src/components/edit-payment/edit-payment.ts
@@ -1,9 +1,52 @@
 Component({
     properties: {
-
+        paymentId: Number
     },
     data: {
-
+        detailInfo: {
+            name: '',
+            idcard: '',
+            bankNumber: '',
+            subBranch: '',
+            isDefault: false
+        },
+        test: 0
+    },
+    lifetimes: {
+        // 组件实例刚刚被创建好时
+        created: function() {
+            console.log('created', this.data, this.data.paymentId);
+        },
+        // 在组件完全初始化完毕、进入页面节点树后
+        attached: function() {
+            console.log('attached', this.data, this.properties.paymentId)
+        },
+        // 在组件在视图层布局完成后执行
+        ready: function() {
+            console.log('ready', this.properties.paymentId)
+        },
+        // 在组件离开页面节点树后
+        detached: () => {
+            console.log('detached')
+        }
+    },
+    observers: {
+      'paymentId': function (val) {
+          console.log(val, '监听paymentId变化');
+          if (val) {
+            // 根据id获取收款信息详情
+              const newData = {
+                  name: '123',
+                  idcard: '41140319930406',
+                  bankNumber: '6217002430001230265',
+                  subBranch: '建设银行',
+                  isDefault: true,
+              }
+            this.setData({
+                detailInfo: newData
+            })
+          }
+      }
     },
     methods: {
         formSubmit(e){
--- src/pages/payment-list/payment-list.ts
+++ src/pages/payment-list/payment-list.ts
@@ -1,13 +1,16 @@
 Page({
    data: {
        isEdit: false,
+       paymentId: null,
        paymentList: [
-           { type: 20, name: '吴笑笑', idcard: '411403199404021678', alipayCode: '15738893656', isDefault: true },
-           { type: 10, name: '吴笑笑', idcard: '411403199404021678', bankNumber: '6217002430001230254', subBranch: '中国银行', isDefault: false },
+           { id: 1, type: 20, name: '吴笑笑', idcard: '411403199404021678', alipayCode: '15738893656', isDefault: true },
+           { id: 2, type: 10, name: '吴笑笑', idcard: '411403199404021678', bankNumber: '6217002430001230254', subBranch: '中国银行', isDefault: false },
        ]
    },
-    editHandle(){
+    editHandle(e: any){
+       const id = e.currentTarget.dataset.id || null;
        this.setData({
+           paymentId: id,
            isEdit: true
        })
     },
