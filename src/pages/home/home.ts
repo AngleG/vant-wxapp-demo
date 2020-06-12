29a96c721ce2b2a0058a0cc81793981a03a1c224
wxx <1464661791@qq.com>
2020-06-12 14:28:57 +0800

    简化home.wxml页面,改用for循环； wxss中不支持标签名、属性、id选择器，改为使用class

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,16 @@
 
 Page({
    data: {
-       userInfo: {}
+       userInfo: {},
+       itemList: [
+           { iconClass: 'icon20_muyue', text: '我的收入', link: 'income' },
+           { iconClass: 'icon_zhundl', text: '参加的访谈', link: 'activities' },
+           { iconClass: 'icon_myshouq', text: '个人信息', link: 'personal' },
+           { iconClass: 'icon_zhanghumx', text: '收款信息', link: 'payment' },
+           { iconClass: 'icon18_sucai', text: '合作流程', link: 'cooperation-process' },
+           { iconClass: 'icon_shouquan', text: '关于智讯', link: 'about-us' },
+       ],
+       paymentInfo: null
    },
     onLoad(){
         const app = getApp();
